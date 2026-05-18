57fab18241dafc2fe90536743e765272a99a878f
Henry Zheng <henry.zheng727@gmail.com>
2026-05-18 15:50:04 -0600

    add putchar, puts, and printf for minimal libc
    printf implementation stolen from OS in 1000 lines

---
+++ lib/stdio.c
@@ -0,0 +1,77 @@
+#include "lib/stdio.h"
+#include "kernel/kernel.h"
+
+void putchar(char c) {
+    if (c == '\n')
+        uart_putc('\r');
+    uart_putc((uint8_t)c);
+}
+
+void puts(const char *s) {
+    while (*s)
+        putchar(*s++);
+    putchar('\n');
+}
+
+void kprintf(const char *fmt, ...) {
+    va_list vargs;
+    va_start(vargs, fmt);
+
+    while (*fmt) {
+        if (*fmt == '%') {
+            fmt++; // Skip '%'
+            switch (*fmt) { // Read the next character
+                case '\0': // '%' at the end of the format string
+                    putchar('%');
+                    goto end;
+                case '%': // Print '%'
+                    putchar('%');
+                    break;
+                case 's': { // Print a NULL-terminated string.
+                    const char *s = va_arg(vargs, const char *);
+                    while (*s) {
+                        putchar(*s);
+                        s++;
+                    }
+                    break;
+                }
+                case 'd': { // Print an integer in decimal.
+                    int value = va_arg(vargs, int);
+                    // Unsigned magnitude so INT_MIN negates correctly.
+                    unsigned magnitude = value;
+                    if (value < 0) {
+                        putchar('-');
+                        magnitude = -magnitude;
+                    }
+
+                    unsigned divisor = 1;
+                    while (magnitude / divisor > 9)
+                        divisor *= 10;
+
+                    while (divisor > 0) {
+                        putchar('0' + magnitude / divisor);
+                        magnitude %= divisor;
+                        divisor /= 10;
+                    }
+
+                    break;
+                }
+                case 'x': { // Print an integer in hexadecimal.
+                    unsigned value = va_arg(vargs, unsigned);
+                    for (int i = 7; i >= 0; i--) {
+                        unsigned nibble = (value >> (i * 4)) & 0xf;
+                        putchar("0123456789abcdef"[nibble]);
+                    }
+                    break;
+                }
+            }
+        } else {
+            putchar(*fmt);
+        }
+
+        fmt++;
+    }
+
+end:
+    va_end(vargs);
+}
